9a035ef9f702e83929189f74668648f0af400b10
Sanil2002 <sanilm4637@gmail.com>
2024-08-19 11:16:08 +0530

    random changes

--- src/Utilities/saveCart.ts
+++ src/Utilities/saveCart.ts
@@ -1,5 +1,3 @@
-// utils/api.ts
-
 type CartItem = {
     id: number;
   title: string;
@@ -13,8 +11,8 @@ type CartItem = {
     const saveCartItemsToLocalStorage = (email: string, cartItems: CartItem[]) => {
     const storedCarts = localStorage.getItem('carts');
     const carts = storedCarts ? JSON.parse(storedCarts) : {};
-    carts[email] = cartItems; // Update the user's cart
-    localStorage.setItem('carts', JSON.stringify(carts)); // Save all carts
+    carts[email] = cartItems;                                                                                       // Update the user's cart
+    localStorage.setItem('carts', JSON.stringify(carts));                                                           // Save all carts
   };
   
   export default saveCartItemsToLocalStorage
